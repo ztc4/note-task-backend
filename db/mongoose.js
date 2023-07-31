ca7ec78e21e2db325d6433b4b57bebac57b5049a
Zachary Coats <96759734+ztc4@users.noreply.github.com>
2023-07-31 15:55:40 -0400

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,4 +2,3 @@ const mongoose = require("mongoose")
 mongoose.set('strictQuery', true)
 mongoose.connect(process.env.DATABASE).then(()=>{console.log("Connected to the database")})
 
-// /Users/zacha/mongodb/bin/mongod.exe --dbpath=/Users/zacha/mongodb-data
